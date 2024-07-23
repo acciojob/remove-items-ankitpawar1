0298cc6257c5f2a0e4e148d7653386e949d6a445
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-07-23 10:26:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeColor() {
+    var select = document.getElementById('colorSelect');
+    var selectedOption = select.options[select.selectedIndex];
+
+    if (selectedOption) {
+        select.remove(selectedOption.index);
+    }
+}
